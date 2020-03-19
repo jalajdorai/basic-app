a93839cd05a0f6a5c66982637e5386f9ad66cb0d
Jalaj <jalajdorai100@gmail.com>
2020-03-19 19:19:02 +0530

    Updates

---
+++ precache-manifest.28640f19ceea1b69cf397cada80efc37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa56c2f1b779ceb293666b0af3887784",
+    "url": "/basic-app/index.html"
+  },
+  {
+    "revision": "f147b1822bc37ad2076a",
+    "url": "/basic-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b02902e35ee2213b1655",
+    "url": "/basic-app/static/js/2.397b9371.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/basic-app/static/js/2.397b9371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f147b1822bc37ad2076a",
+    "url": "/basic-app/static/js/main.3e48e2df.chunk.js"
+  },
+  {
+    "revision": "5dc40ae30a58bf139eb6",
+    "url": "/basic-app/static/js/runtime-main.08a975bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basic-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
